87436e50fda749e6c5c14054d30c98e34fa7a230
yn10 <shjaewon.dev@gmail.com>
2020-04-27 16:10:25 +0900

    제이쿼리3일차 : 홈페이지 메뉴 만들기 css html jquery

---
+++ html5/WebContent/WebContent/js/jquery002.js
@@ -0,0 +1,20 @@
+$(function() {
+	$(".menuBtn").click(function() {
+		if($("nav").hasClass("navOn")){
+			$("nav").removeClass("navOn");
+			$(".overlay").css({"width" : "0" , "height" : "0" , "cursor" : "pointer" });
+			$(".menuBtn > span").removeClass("menuBtnOn");		
+		}else{
+			
+			$("nav").addClass("navOn");
+			$(".overlay").css({"width" : "100%" , "height" : "100%" , "cursor" : "pointer" });
+			console.log("메뉴");
+			$(".menuBtn > span").addClass("menuBtnOn");		
+			
+		}
+	}); 
+	
+	$(".overlay").click(function() {
+		$(".menuBtn").trigger("click");
+	});
+});
