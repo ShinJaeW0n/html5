7657fa94a28433bdaf45cc8f4a53ecab801fc826
yn10 <shjaewon.dev@gmail.com>
2020-05-07 16:04:55 +0900

    jsp 4일차: 빈테스트 예제들

---
+++ jspStudy/src/com/test/jsp/dto/beanTest.java
@@ -0,0 +1,25 @@
+package com.test.jsp.dto;
+
+public class beanTest {
+	private String name;
+
+	public beanTest() {
+		
+	}
+	
+	public beanTest(String name) {
+		super();
+		this.name = name;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
